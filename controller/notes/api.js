25a1896e0516c9210f1f5738abc04ab932d8c726
watashiox14 <watashiox123@gmail.com>
2023-12-02 15:32:01 +0700

    delete , update note

--- controller/notes/api.js
+++ controller/notes/api.js
@@ -88,3 +88,92 @@ export const getNote = async (req,res)=>{
         console.error('error',error)
     }
 }
+
+export const deleteNote = async(req,res)=>{
+    try {
+        const {notes_id} = req.body
+        const cookie = await req.cookies
+        const token = cookie.token
+        if(!token){
+            return res.status(404).json({
+                status: 'fail',
+                message: 'you must login!'
+            })
+        }
+        jwt.verify(token,process.env.JWT_TOKEN, async(error,decoded)=>{
+            if(error){
+                return res.status(404).json({
+                    status: 'fail',
+                    message: 'error',error
+                })
+            }
+            const username = decoded.username
+            const user = await findUsername(username)
+            const id = user.id
+            const result = await findNote({
+                where:{
+                    notes_id
+                }
+            })
+            if(result){
+                await result.destroy({where:{notes_id}})
+                return res.status(200).json({
+                    status: 'success',
+                    message: 'success delete!'
+                })
+            }
+        })
+    } catch (error) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'error',error
+        })
+    }
+}
+
+export const updateNote = async(req,res)=>{
+try {
+    const {notes_id,checked,notes_name,notes_desc,due} = req.body
+    const cookie = await req.cookies
+    const token = cookie.token
+    if(!token){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'you must login!'
+        })
+    }
+    jwt.verify(token,process.env.JWT_TOKEN, async(error,decoded)=>{
+        if(error){
+            return res.status(404).json({
+                status: 'fail',
+                message: 'error',error
+            })
+        }
+        const username = decoded.username
+        const user = await findUsername(username)
+        const id = user.id
+        const result = await findNote({
+            where:{
+                notes_id
+            }
+        })
+        if(result){
+            await result.update({
+                checked,
+                notes_name,
+                notes_desc,
+                due
+            })
+            return res.status(200).json({
+                status: 'success',
+                message: 'success update!'
+            })
+        }
+    })
+} catch (error) {
+    return res.status(404).json({
+        status: 'fail',
+        message: 'error',error
+    })
+}
+}
