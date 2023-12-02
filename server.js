09179b35bccc85d98f64cc366c3d7bbbdfdfa8d7
watashiox14 <watashiox123@gmail.com>
2023-12-02 18:29:00 +0700

    add cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from "cors"
 import router from "./router/routes.js";
 
 
-app.use(cors({origin: 'https://todo-client-mqxn4q5g2q-as.a.run.app', credentials: true}))
+app.use(cors({origin: 'https://todo-api-mqxn4q5g2q-as.a.run.app', credentials: true}))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded({extended: false}));
